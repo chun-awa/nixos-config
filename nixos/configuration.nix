652b8fe36c26414c9d48155a765e01cf373ae2ef
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-10-20 20:16:24 +0800

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -91,9 +91,6 @@
     settings = {
       # Opinionated: forbid root login through SSH.
       PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
-      PasswordAuthentication = false;
     };
   };
 
