1b9c4a437ea3e3c1138002e3c2093b0ac6334c34
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-10-23 06:11:43 +0800

    Update disko.nix

--- disko.nix
+++ disko.nix
@@ -15,7 +15,6 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot/efi";
-                mountOptions = [ "umask=0077" ];
               };
             };
             nixos = {
