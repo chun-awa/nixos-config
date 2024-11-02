25249a05b7c4ac7db5253ed1bd8d9e8f0bed14c1
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-11-02 10:26:58 +0800

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -39,7 +39,6 @@
     # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
 
