002279c6e7d6712fc3f5238c1e55d854ae8a2da5
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-11-02 22:33:50 +0800

    Move modules to core/

---
+++ modules/nixos/core/default.nix
@@ -0,0 +1,10 @@
+{
+  imports = [
+    ./nix.nix
+    ./networking.nix
+    ./filesystem.nix
+    ./ssh.nix
+    ./i18n.nix
+    ./console.nix
+  ];
+}
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,15 +1,9 @@
 {
   imports = [
-    ./nix.nix
-    ./networking.nix
-    ./filesystem.nix
-    ./ssh.nix
     ./audio.nix
-    ./i18n.nix
     ./zram.nix
     ./virtualisation.nix
     ./wine.nix
-    ./console.nix
     ./amdvlk.nix
   ];
 }
