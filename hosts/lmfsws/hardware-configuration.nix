799eb875aa964d17ad89422472a0b4942422b226
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-11-10 18:24:11 +0800

    refractor: directory structure

---
+++ hosts/lmfsws/hardware-configuration.nix
@@ -0,0 +1,8 @@
+{
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
