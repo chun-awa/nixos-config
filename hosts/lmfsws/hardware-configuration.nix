19a75621165f790ebdcfd95656bfa6694e025c18
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-11-15 21:56:46 +0800

    Update hardware-configuration.nix

--- hosts/lmfsws/hardware-configuration.nix
+++ hosts/lmfsws/hardware-configuration.nix
@@ -1,4 +1,7 @@
 {
+  lib,
+  ...
+}: {
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
