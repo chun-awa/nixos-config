492c2d9649585c39ff3e713ce22160eafcbb848d
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-11-17 15:43:55 +0800

    Update configuration.nix

--- hosts/lmfsws/configuration.nix
+++ hosts/lmfsws/configuration.nix
@@ -9,17 +9,17 @@
   ...
 }: {
   # You can import other NixOS modules here
-  imports = [
-    outputs.nixosModules
-    ./hardware-configuration.nix
-    ../../users/chun
-  ] ++ with inputs.nixos-hardware.nixosModules; [
+  imports = with inputs.nixos-hardware.nixosModules; [
     common-cpu-amd
     common-cpu-amd-pstate
     common-gpu-amd
     common-pc-laptop
     common-pc-laptop-acpi_call
     common-pc-laptop-ssd
+  ] ++ [
+    outputs.nixosModules
+    ./hardware-configuration.nix
+    ../../users/chun
   ];
   networking.hostName = "lmfsws";
   nixpkgs = {
