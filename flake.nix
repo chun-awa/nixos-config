16f08f33b729379a8889e2bc862b7f19acec211e
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-11-19 21:18:27 +0800

    fix

--- flake.nix
+++ flake.nix
@@ -25,12 +25,12 @@
     ...
   } @ inputs: let
     inherit (self) outputs;
-    inherit (inputs.nixpkgs) lib;
-    mylib = import ../lib {inherit lib;};
     # Supported systems for your flake packages, shell, etc.
     systems = [
       "x86_64-linux"
     ];
+    inherit (nixpkgs) lib;
+    mylib = import ../lib {inherit lib;};
     # This is a function that generates an attribute by calling a function you
     # pass to it, with each system as an argument
     forAllSystems = nixpkgs.lib.genAttrs systems;
