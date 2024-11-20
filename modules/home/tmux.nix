3dbaf62d56ae0d8d4547d4bb1e50d01b95c7004a
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-11-20 20:53:29 +0800

    fix

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -6,5 +6,5 @@
     enable = true;
     extraConfig = builtins.readFile "${inputs.tmux-config}/.tmux.conf";
   };
-  home.file.".config/tmux/.tmux.conf.local".source = "${inputs.dotfiles}/.tmux.conf.local";
+  home.file.".config/tmux/tmux.conf.local".source = "${inputs.dotfiles}/.tmux.conf.local";
 }
