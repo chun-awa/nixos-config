d36f7b0ce62c4f38278581f369de87771099b18e
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-11-26 21:30:24 +0800

    fix: fcitx5

--- modules/home/applications/fcitx5.nix
+++ modules/home/applications/fcitx5.nix
@@ -3,8 +3,7 @@
   ...
 }: {
   i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
+    enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-chinese-addons
       fcitx5-gtk
