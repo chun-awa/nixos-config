1f6ae3dff01501d1764e6dddf586e34d06bbba70
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-11-28 20:35:03 +0800

    feat: v4l2loopback

--- modules/nixos/core/kernel.nix
+++ modules/nixos/core/kernel.nix
@@ -1,7 +1,12 @@
 {
   pkgs,
+  config,
   ...
 }: {
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
-  boot.initrd.systemd.enable = true;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_xanmod;
+    extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ]
+    kernelModules = [ "v4l2loopback" ];
+    initrd.systemd.enable = true;
+  }
 }
