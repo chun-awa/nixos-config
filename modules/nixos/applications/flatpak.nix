cfebe873be95c5e2116938e5d38dd0975db6fe9d
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-11-28 22:46:16 +0800

    fix: use builtins.fetchurl

--- modules/nixos/applications/flatpak.nix
+++ modules/nixos/applications/flatpak.nix
@@ -5,7 +5,7 @@
       {
         name = "flathub";
         location = "https://mirror.sjtu.edu.cn/flathub";
-        gpg-import = fetchurl "https://mirror.sjtu.edu.cn/flathub/flathub.gpg";
+        gpg-import = builtins.fetchurl "https://mirror.sjtu.edu.cn/flathub/flathub.gpg";
       }
     ];
     update.auto = {
