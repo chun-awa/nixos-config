4e7e285ba2792064d5372aae2cc55012498c5898
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-11-28 20:22:34 +0800

    feat: zenpower

--- hosts/lmfsws/hardware-configuration.nix
+++ hosts/lmfsws/hardware-configuration.nix
@@ -1,12 +1,15 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot.extraModulePackages = with pkgs.linuxKernel.packages.linux_xanmod_latest; [
+    zenpower
+  ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
