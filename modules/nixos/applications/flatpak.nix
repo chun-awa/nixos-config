fdc02af0f38e41e2f752fd42e4f08e12e9ce0fb0
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-11-29 19:36:19 +0800

    fix: flathub.gpg sha256

--- modules/nixos/applications/flatpak.nix
+++ modules/nixos/applications/flatpak.nix
@@ -10,7 +10,7 @@
         location = "https://mirror.sjtu.edu.cn/flathub";
         gpg-import = pkgs.fetchurl {
           url = "https://mirror.sjtu.edu.cn/flathub/flathub.gpg";
-          sha256 = "";
+          sha256 = "sha256-i9wgq8ThnAeWRgvrW/4OeqQThxaZnhnG8tvdeMxBrqo=";
         };
       }
     ];
