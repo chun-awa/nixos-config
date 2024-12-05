a620c3bf1405466601c90d231d462153918133b1
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-05 21:29:35 +0800

    feat: bring back wine

--- hosts/lmfsws/configuration.nix
+++ hosts/lmfsws/configuration.nix
@@ -37,6 +37,7 @@
       "modules/nixos/desktop/audio.nix"
       "modules/nixos/applications/pkgs.nix"
       "modules/nixos/applications/flatpak.nix"
+      "modules/nixos/applications/wine.nix"
       "modules/nixos/virtualisation/qemu.nix"
       "modules/nixos/virtualisation/docker.nix"
       "users/chun"
---
+++ modules/nixos/applications/wine.nix
@@ -0,0 +1,10 @@
+{
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    wineWowPackages.wayland
+    wineWowPackages.fonts
+    winetricks
+  ];
+}
