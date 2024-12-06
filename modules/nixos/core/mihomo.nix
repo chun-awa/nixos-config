7825d71bfbcaa3f516e2e916183f2ebf158bf7d4
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-06 22:02:16 +0800

    fix: syntax error

--- modules/nixos/core/mihomo.nix
+++ modules/nixos/core/mihomo.nix
@@ -2,22 +2,21 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  configPath = "/root/.config/mihomo/config.yaml";
+in {
   services.mihomo = {
     enable = true;
     webui = pkgs.metacubexd;
-    let
-      configPath = "/root/.config/mihomo/config.yaml";
-    in
-      lib.mkMerge [
-        lib.mkIf (builtins.pathExists configPath) {
-          configFile = configPath;
-        }
-        lib.mkIf (!builtins.pathExists configPath) {
-          configFile = pkgs.writeText "config.yaml" ''
-            mixed-port: 7890
-          '';
-        }
-      ];
+    lib.mkMerge [
+      lib.mkIf (builtins.pathExists configPath) {
+        configFile = configPath;
+      }
+      lib.mkIf (!builtins.pathExists configPath) {
+        configFile = pkgs.writeText "config.yaml" ''
+          mixed-port: 7890
+        '';
+      }
+    ];
   };
 }
