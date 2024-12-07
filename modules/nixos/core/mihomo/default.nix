6d6dea22318cf6738fde871adc506327c7ea16d6
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-07 21:44:09 +0800

    fix(mihomo): use built-in if

---
+++ modules/nixos/core/mihomo/default.nix
@@ -0,0 +1,13 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  services.mihomo = {
+    enable = true;
+    webui = pkgs.metacubexd;
+    configFile = let
+      configPath = /root/.config/mihomo/config.yaml;
+      in if (builtins.pathExists configPath) then configPath else ./config.yaml;
+  };
+}
