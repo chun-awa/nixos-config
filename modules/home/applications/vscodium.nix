0d1ee8312dae4851721a24af790e1ac3804577e9
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-08 19:04:45 +0800

    feat(vscodium): add plugins

--- modules/home/applications/vscodium.nix
+++ modules/home/applications/vscodium.nix
@@ -7,6 +7,17 @@
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
+      ms-python.python
+      ms-python.vscode-pylance
+      ms-python.black-formatter
+      Vue.volar
+      foxundermoon.shell-format
+      timonwong.shellcheck
+      tamasfe.even-better-toml
+      davidanson.vscode-markdownlint
+      vivaxy.vscode-conventional-commits
+      WakaTime.vscode-wakatime
+      LeonardSSH.vscord
     ];
   };
 }
