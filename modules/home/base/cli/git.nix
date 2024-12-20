875e24ee7addfed91bae1465ac881372a6a71d47
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-20 21:14:13 +0800

    feat(modules/home): git

---
+++ modules/home/base/cli/git.nix
@@ -0,0 +1,8 @@
+{
+  programs.git = {
+    enable = true;
+    # TODO: make this not hard-coded
+    userName = "chun-awa";
+    userEmail = "110163919+chun-awa@users.noreply.github.com";
+  };
+}
--- users/chun/lmfsws.nix
+++ users/chun/lmfsws.nix
@@ -18,13 +18,8 @@
     username = "chun";
     homeDirectory = "/home/chun";
   };
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
 
-  # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
