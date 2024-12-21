a18215094a070cb0cae94bd00aad88d9cf538be4
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-21 14:18:51 +0800

    feat(overlays): disable unfree software

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,7 +15,7 @@
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
       system = final.system;
-      config.allowUnfree = true;
+      config.allowUnfree = false;
     };
   };
 }
