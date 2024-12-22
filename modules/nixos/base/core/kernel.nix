33138ba405f05cab7029603daeb33af044b72622
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-23 06:16:45 +0800

    feat(modules/nixos/kernel): obs virtual camera

--- modules/nixos/base/core/kernel.nix
+++ modules/nixos/base/core/kernel.nix
@@ -7,6 +7,9 @@
     kernelPackages = pkgs.linuxPackages_xanmod;
     extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
     kernelModules = ["v4l2loopback"];
+    boot.extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
     initrd.systemd.enable = true;
     kernel.sysctl = {
       "vm.swappiness" = 100;
