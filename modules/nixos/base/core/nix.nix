547648a638961b59439286afb7f2a517750f7a10
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-23 20:38:55 +0800

    feat(modules/nixos/nix): add nix-community cache

--- modules/nixos/base/core/nix.nix
+++ modules/nixos/base/core/nix.nix
@@ -13,6 +13,10 @@
       auto-optimise-store = true;
       substituters = [
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
