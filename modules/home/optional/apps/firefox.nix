d379aba59ac3fb37fca93428a119ba7a2ca889c1
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-23 21:45:10 +0800

    feat(modules/home/apps): firefox

---
+++ modules/home/optional/apps/firefox.nix
@@ -0,0 +1,36 @@
+{
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      default = {
+        id = 0;
+        name = "default";
+        isDefault = true;
+        settings = {
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.shell.checkDefaultBrowser" = false;
+          "privacy.donottrackheader.enabled" = true;
+          "privacy.trackingprotection.enabled" = true;
+          "privacy.trackingprotection.socialtracking.enabled" = true;
+          "privacy.partition.network_state.ocsp_cache" = true;
+        };
+        policies = {
+          DisableTelemetry = true;
+          DisableFirefoxStudies = true;
+          DisablePocket = true;
+          DisableFirefoxAccounts = true;
+          DisableAccounts = true;
+          DisableFirefoxScreenshots = true;
+          FirefoxHome = {
+            Pocket = false;
+            Snippets = false;
+          };
+          UserMessaging = {
+            ExtensionRecommendation = false;
+            SkipOnboarding = false;
+          };
+        };
+      };
+    };
+  };
+}
