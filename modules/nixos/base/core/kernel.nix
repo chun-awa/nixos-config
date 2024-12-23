3c7c36fea2037aea5653bdd5136b02c88bcffe4c
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-12-23 21:25:22 +0800

    fix(modules/nixos/kernel): The option `boot.boot' does not exist

--- modules/nixos/base/core/kernel.nix
+++ modules/nixos/base/core/kernel.nix
@@ -7,7 +7,7 @@
     kernelPackages = pkgs.linuxPackages_xanmod;
     extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
     kernelModules = ["v4l2loopback"];
-    boot.extraModprobeConfig = ''
+    extraModprobeConfig = ''
       options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
     '';
     initrd.systemd.enable = true;
