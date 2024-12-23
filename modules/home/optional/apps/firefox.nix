9ac1177a8bc75d77a120d20c30984a71b6d2adb6
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-23 21:52:55 +0800

    fix(modules/home/apps/firefox): The option `home-manager.users.chun.programs.firefox.profiles.default.policies' does not exist

--- modules/home/optional/apps/firefox.nix
+++ modules/home/optional/apps/firefox.nix
@@ -1,6 +1,22 @@
 {
   programs.firefox = {
     enable = true;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+      FirefoxHome = {
+        Pocket = false;
+        Snippets = false;
+      };
+      UserMessaging = {
+        ExtensionRecommendation = false;
+        SkipOnboarding = false;
+      };
+    };
     profiles = {
       default = {
         id = 0;
@@ -13,22 +29,6 @@
           "privacy.trackingprotection.enabled" = true;
           "privacy.trackingprotection.socialtracking.enabled" = true;
           "privacy.partition.network_state.ocsp_cache" = true;
-        };
-        policies = {
-          DisableTelemetry = true;
-          DisableFirefoxStudies = true;
-          DisablePocket = true;
-          DisableFirefoxAccounts = true;
-          DisableAccounts = true;
-          DisableFirefoxScreenshots = true;
-          FirefoxHome = {
-            Pocket = false;
-            Snippets = false;
-          };
-          UserMessaging = {
-            ExtensionRecommendation = false;
-            SkipOnboarding = false;
-          };
         };
       };
     };
