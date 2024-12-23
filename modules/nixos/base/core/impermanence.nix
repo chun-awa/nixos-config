5fb29f14af336eede9ef0411d2f2458afb73f5dc
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-23 22:09:44 +0800

    feat(modules/nixos/impermanence): add workaround to bind mount /etc/shadow properly

--- modules/nixos/base/core/impermanence.nix
+++ modules/nixos/base/core/impermanence.nix
@@ -1,5 +1,7 @@
-{
-  environment.persistence."/persistent" = {
+let
+  persistentPath = "/persistent";
+in {
+  environment.persistence."${persistentPath}" = {
     enable = true;
     hideMounts = true;
     directories = [
@@ -10,5 +12,32 @@
       "/etc/machine-id"
       "/etc/shadow"
     ];
+  };
+  # hacks to prevent failed bind mount on /etc/shadow
+  # https://github.com/nix-community/impermanence/issues/120#issuecomment-2382674299
+  system.activationScripts = {
+    etc_shadow = ''
+      [ -f "/etc/shadow" ] && cp /etc/shadow ${persistentPath}/etc/shadow
+      [ -f "${persistentPath}/etc/shadow" ] && cp ${persistentPath}/etc/shadow /etc/shadow
+    '';
+
+    users.deps = ["etc_shadow"];
+  };
+
+  systemd.services."etc_shadow_persistence" = {
+    enable = true;
+    description = "Persist /etc/shadow on shutdown.";
+    wantedBy = ["multi-user.target"];
+    path = [pkgs.util-linux];
+    unitConfig.defaultDependencies = true;
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      # Service is stopped before shutdown
+      ExecStop = pkgs.writeShellScript "persist_etc_shadow" ''
+        mkdir -p "${persistentPath}/etc"
+        cp /etc/shadow ${persistentPath}/etc/shadow
+      '';
+    };
   };
 }
