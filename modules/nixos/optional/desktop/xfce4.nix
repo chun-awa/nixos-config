5a89b7856a85446f4d47f22dcb9e9e5af5481cea
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-25 22:28:14 +0800

    feat(nixos/desktop): xfce4

---
+++ modules/nixos/optional/desktop/xfce4.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }: {
+  environment.xfce.excludePackages = with pkgs.xfce; [
+    mousepad
+    parole
+    ristretto
+    xfce4-screenshooter
+    xfce4-taskmanager
+    xfce4-terminal
+    xfwm4-themes
+    xfce4-screensaver
+  ];
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce.enable = true;
+    };
+  };
+}
