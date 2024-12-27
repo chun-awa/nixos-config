37b2ad9eb4f8f44f7cd635d172c71f5a63f4a8b6
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-27 20:43:46 +0800

    feat(qemu): spice

--- modules/nixos/optional/virt/qemu.nix
+++ modules/nixos/optional/virt/qemu.nix
@@ -1,15 +1,25 @@
 {pkgs, ...}: {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      ovmf.enable = true;
-      ovmf.packages = [pkgs.OVMFFull.fd];
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [pkgs.OVMFFull.fd];
+      };
     };
+    spiceUSBRedirection.enable = true;
   };
+  services.spice-vdagentd.enable = true;
   environment.systemPackages = with pkgs; [
     virt-manager
     virt-viewer
     qemu_kvm
     qemu
+    spice
+    spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
   ];
 }
