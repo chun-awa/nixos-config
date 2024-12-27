fb7ad5c91a0aff2a9bf9c894885245accb1a076f
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-27 21:21:16 +0800

    feat(bat): theme & extraPackages

--- modules/home/optional/cli/bat.nix
+++ modules/home/optional/cli/bat.nix
@@ -1,5 +1,15 @@
 {
   programs.bat = {
     enable = true;
+    config = {
+      pager = "less -FR";
+      theme = "Monokai Extended";
+    };
+    extraPackages = with pkgs.bat-extras; [
+      batman
+      batpipe
+      batgrep
+      batdiff
+    ];
   };
 }
