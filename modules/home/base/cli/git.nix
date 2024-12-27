6e8a230351e5d40f620ee3bfd84f30d49c620824
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-12-27 20:58:36 +0800

    feat(git): lfs

--- modules/home/base/cli/git.nix
+++ modules/home/base/cli/git.nix
@@ -1,6 +1,7 @@
 {
   programs.git = {
     enable = true;
+    lfs.enable = true;
     # TODO: make this not hard-coded
     userName = "chun-awa";
     userEmail = "110163919+chun-awa@users.noreply.github.com";
