0357fd28536486e425ab0941e056addea456445a
chun-awa <110163919+chun-awa@users.noreply.github.com>
2025-01-01 20:06:22 +0800

    chore(hosts): remove lmfsws

--- users/chun/test-qemuvm.nix
+++ users/chun/test-qemuvm.nix
@@ -1,1 +1,29 @@
-lmfsws.nix
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  utils,
+  mylib,
+  ...
+}: {
+  imports = lib.flatten [
+    (mylib.listNixFiles (mylib.relativeToRoot "modules/home/base"))
+    (mylib.excludeModules "modules/home/optional" [
+      ])
+  ];
+
+  home = {
+    username = "chun";
+    homeDirectory = "/home/chun";
+  };
+
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.11";
+}
