425fc0c36be288da02e1899981fb45a65bbdc905
Canadew <110163919+chun-awa@users.noreply.github.com>
2025-03-09 16:55:04 +0800

    feat: add zram parameters

--- modules/nixos/base/core/kernel.nix
+++ modules/nixos/base/core/kernel.nix
@@ -7,8 +7,12 @@
   boot = {
     kernelPackages = pkgs.linuxPackages_xanmod;
     initrd.systemd.enable = true;
+    # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
     kernel.sysctl = {
-      "vm.swappiness" = 100;
+      "vm.swappiness" = 180;
+      "vm.watermark_boost_factor" = 0;
+      "vm.watermark_scale_factor" = 125;
+      "vm.page-cluster" = 0;
     };
   };
 }
