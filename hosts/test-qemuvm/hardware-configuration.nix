3a2d72aead732049a5f875dac7338ecf6efa8087
chun-awa <110163919+chun-awa@users.noreply.github.com>
2025-08-20 14:38:13 +0800

    refactor: move fileSystems to hardware-configuration

--- hosts/test-qemuvm/hardware-configuration.nix
+++ hosts/test-qemuvm/hardware-configuration.nix
@@ -11,4 +11,35 @@
   boot.extraModulePackages = [];
   boot.loader.grub2-theme.customResolution = "1280x800";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  fileSystems = let
+    rootFilesystem = {subvol}: {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = [
+        "noatime"
+        "compress=zstd"
+        "subvol=${subvol}"
+      ];
+    };
+  in
+    {
+      "/" = {
+        device = "none";
+        fsType = "tmpfs";
+        options = ["defaults" "size=25%" "mode=755"];
+      };
+      "/btrfs_root" = {
+        device = "/dev/disk/by-label/nixos";
+        fsType = "btrfs";
+        options = ["subvolid=5"];
+      };
+      "/persistent" =
+        (rootFilesystem {subvol = "/persistent";})
+        // {
+          neededForBoot = true;
+        };
+    }
+    // lib.genAttrs ["/nix" "/var" "/home" "/swap" "/boot"] (subvol: rootFilesystem {inherit subvol;});
+  swapDevices = [{device = "/swap/swapfile";}];
 }
