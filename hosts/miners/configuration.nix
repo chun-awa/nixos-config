c91f747755e36ac5017f0f811aaeb1be38addc4a
chun-awa <110163919+chun-awa@users.noreply.github.com>
2025-08-21 15:44:47 +0800

    fix: disable wine

--- hosts/miners/configuration.nix
+++ hosts/miners/configuration.nix
@@ -24,6 +24,7 @@
       ])
       (mylib.appendPath "apps" [
         "flatpak.nix"
+        "wine.nix"
       ])
     ])
     (mylib.appendPath (mylib.relativeToRoot "users") [
--- users/chun/test-qemuvm.nix
+++ users/chun/test-qemuvm.nix
@@ -13,7 +13,6 @@
     (mylib.excludeModules "modules/home/optional" [
       (mylib.appendPath "apps" [
         "flatpak.nix"
-        "wine.nix"
       ])])
   ];
 
