3b1d5b3b76233957177e8ea0751fc285a0c7b845
chun-awa <110163919+chun-awa@users.noreply.github.com>
2025-08-21 21:50:22 +0800

    feat: hyprland

---
+++ modules/home/optional/desktop/hyprland.nix
@@ -0,0 +1,72 @@
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    swaybg
+    waybar
+    flameshot
+  ]
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+
+    settings = {
+      "$mainMod" = "SUPER";
+
+      monitor = ", preferred, auto, 1";
+
+      input = { 
+        touchpad = {
+          natural_scroll = true;
+        };
+      };
+
+      exec-once = [
+        "swaybg -i ~/Pictures/wallpaper.png"
+        "waybar"
+      ];
+
+      bind = [
+        "$mainMod, Return, exec, alacritty"
+        "$mainMod, Q, killactive,"
+        "Alt_L, F4, exit,"
+        "$mainMod, F, togglefloating,"
+
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
+        "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
+        "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
+        "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
+        "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
+        "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
+        "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
+        "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
+        "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
+        "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
+
+        ", XF86AudioRaiseVolume, exec, pamixer -i 5 "
+        ", XF86AudioLowerVolume, exec, pamixer -d 5 "
+        ", XF86AudioMute, exec, pamixer -t"
+        ", XF86AudioMicMute, exec, pamixer --default-source -m"
+        
+        ", XF86MonBrightnessDown, exec, brightnessctl set 5%- "
+        ", XF86MonBrightnessUp, exec, brightnessctl set +5% "
+
+        '', Print, exec, flameshot full -c''
+      ];
+
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+    };
+  };
+}
